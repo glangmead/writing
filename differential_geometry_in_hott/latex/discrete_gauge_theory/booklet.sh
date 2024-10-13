972bbb8d27f41be1605119d7a21fa0a497112ede
Greg Langmead <glangmead@gmail.com>
2024-10-13 17:53:11 -0400

    lots of writing today

---
+++ differential_geometry_in_hott/latex/discrete_gauge_theory/booklet.sh
@@ -0,0 +1,3 @@
+python ~/proj/PDFsuite/Python3/cropPDF.py discrete_gauge_theory.pdf
+python ~/proj/PDFsuite/Python3/bookletPDF.py discrete_gauge_theory\ TPS.pdf
+open discrete_gauge_theory\ TPS\ booklet.pdf
--- differential_geometry_in_hott/latex/discrete_gauge_theory/to_html.sh
+++ differential_geometry_in_hott/latex/discrete_gauge_theory/to_html.sh
@@ -7,14 +7,15 @@ do
   latexpand $i.tex > tmp/$i.copyincludes.tex
   cd tmp
   cp ../greg.sty ../agda.sty ../quiver.sty ../$i.yml ../ima.csl ../connections.bib ../tikz.py .
-  /opt/homebrew/Cellar/pandoc/3.1.11.1/bin/pandoc --filter tikz.py --from latex+raw_tex $i.copyincludes.tex -o $i.tikzimg.tex
+  ../pandoc-3.1.13-arm64/bin/pandoc --filter tikz.py --from latex+raw_tex $i.copyincludes.tex -o $i.tikzimg.tex
   echo "\usepackage{greg}" > foo && cat $i.tikzimg.tex >> foo && mv foo $i.tikzimg.tex
-  /opt/homebrew/Cellar/pandoc/3.1.11.1/bin/pandoc --from latex+latex_macros --mathjax \
+  ../pandoc-3.1.13-arm64/bin/pandoc --from latex+latex_macros --mathjax \
     -M document-css=false --metadata-file=$i.yml \
     --toc --number-sections --number-offset=0 --standalone --citeproc -M link-citations=true \
     -V caption-justification:centering \
     $i.tikzimg.tex -t html -o ../html/$i.html
   cp *.png ../html
   cd ..
+  cp dense_circle.pdf html
   open html/$i.html
 done
