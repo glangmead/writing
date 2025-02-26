8bebf16a93617f61277d3dea9bb37f7b106bab3b
Greg Langmead <glangmead@gmail.com>
2025-02-26 09:02:28 -0500

    some html fixes

--- differential_geometry_in_hott/latex/discrete_gauge_theory/to_html.sh
+++ differential_geometry_in_hott/latex/discrete_gauge_theory/to_html.sh
@@ -8,9 +8,9 @@ do
   latexpand $i.tex > tmp/$i.copyincludes.tex
   cd tmp
   cp ../greg.sty ../agda.sty ../quiver.sty ../$i.yml ../ima.csl ../connections.bib ../tikz.py .
-  ../pandoc-3.1.13-arm64/bin/pandoc --filter tikz.py --from latex+raw_tex $i.copyincludes.tex -o $i.tikzimg.tex
+  ../pandoc-3.1.13-arm64/bin/pandoc --wrap=none --filter tikz.py --from latex+raw_tex $i.copyincludes.tex -o $i.tikzimg.tex
   echo "\usepackage{greg}" > foo && cat $i.tikzimg.tex >> foo && mv foo $i.tikzimg.tex
-  ../pandoc-3.1.13-arm64/bin/pandoc --from latex+latex_macros --mathjax \
+  ../pandoc-3.1.13-arm64/bin/pandoc --wrap=none --from latex+latex_macros --mathjax \
     -M document-css=false --metadata-file=$i.yml \
     --toc --number-sections --number-offset=0 --standalone --citeproc -M link-citations=true \
     -V caption-justification:centering \
@@ -18,7 +18,7 @@ do
   cp -f *.png *.jpg ../html || :
   cd ..
   cp -f *.png *.jpg html || :
-  cp dense_circle.pdf html
+  cp dense_circle.pdf triangulated_genus3.pdf triangulated_sphere.pdf html
   cat html/$i.html | sed 's/embed src="\(.*\).pdf"/img src="\1.png"/' | sed 's/0◻//g' | sed 's=<p><em><span>2</span></em></p>==g' > html/foo.html
   mv html/foo.html html/$i.html
   open html/$i.html
