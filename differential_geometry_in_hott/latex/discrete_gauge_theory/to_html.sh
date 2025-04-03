c99c199c9aacabf2484ca101bf0fbd72ecfbba2c
Greg Langmead <glangmead@gmail.com>
2025-04-03 10:24:20 -0400

    refactored some tikz into files for reuse in the dgt_talk deck; fixed html conversion of the vector field data tables using a pandoc_only_macros.tex file

--- differential_geometry_in_hott/latex/discrete_gauge_theory/to_html.sh
+++ differential_geometry_in_hott/latex/discrete_gauge_theory/to_html.sh
@@ -7,14 +7,14 @@ for i in discrete_gauge_theory
 do
   latexpand $i.tex > tmp/$i.copyincludes.tex
   cd tmp
-  cp ../greg.sty ../agda.sty ../quiver.sty ../$i.yml ../aims-mathematics.csl ../connections.bib ../tikz.py .
+  cp ../greg.sty ../agda.sty ../quiver.sty ../$i.yml ../aims-mathematics.csl ../connections.bib ../tikz.py ../pandoc_only_macros.tex .
   ../pandoc-3.1.13-arm64/bin/pandoc --wrap=none --filter tikz.py --from latex+raw_tex $i.copyincludes.tex -o $i.tikzimg.tex
   echo "\usepackage{greg}" > foo && cat $i.tikzimg.tex >> foo && mv foo $i.tikzimg.tex
   ../pandoc-3.1.13-arm64/bin/pandoc --wrap=none --from latex+latex_macros --mathjax \
     -M document-css=false --metadata-file=$i.yml \
     --toc --number-sections --number-offset=0 --standalone --citeproc -M link-citations=true \
     -V caption-justification:centering \
-    $i.tikzimg.tex -t html -o ../html/$i.html
+    pandoc_only_macros.tex $i.tikzimg.tex -t html -o ../html/$i.html
   cp -f *.png *.jpg *.svg ../html || :
   cd ..
   cp -f *.png *.jpg *.svg html || :
